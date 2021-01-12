a1e93ffd23db8ff0aa8a09425c824e41712210fa
cyChopp <45396059+cyChopp@users.noreply.github.com>
2021-01-12 13:31:33 +0100

    Add responsive navbar

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -3,14 +3,15 @@ import './Feed.css'
 
 const Feed = () => {
     return (
-        
+        <div className='nameWrapper'>
          <div className='name'>
-        <h1 className=" first" data-text="ALEX">
+        <h1 className=" first firstname" >
           ALEX . . . . . . .
           </h1>
           <h1 className="text first" data-text="LOUTTCHENKO">
           LOUTTCHENKO
           </h1>
+        </div>
         </div>
     )
 }
--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,20 +1,75 @@
-import React, { useState } from 'react'
-import './NavBar.css'
-import MenuIcon from '@material-ui/icons/Menu';
+import React, { useState } from "react";
+import "./NavBar.css";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const NavBar = () => {
-    const [isOpen,setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleClick = ()=>{
-        setIsOpen(!isOpen)
-    }
-    return (
-        <>
-        <nav className="navbarItems">
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-      </nav>
-      </>
-    )
-}
+  const openNav = () => {
+    document.getElementById("mySidenav").style.width = "100%";
+  };
 
-export default NavBar
+  const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+  };
+  return (
+    <>
+      <div className="fixedNavbarWrapper">
+        <div className="grow" />
+            <div className="navbarLinks">
+            <ul>
+                <li>
+                <a href="#">PROJECTS</a>
+                </li>
+                <li>
+                <a href="#">SKILLS</a>
+                </li>
+                <li>
+                <a href="#">EXPERIENCE</a>
+                </li>
+                <li>
+                <a href="#">CONTACT ME</a>
+                </li>
+            </ul>
+            </div>
+        <div className="grow" />
+      </div>
+ 
+      <div id="mySidenav" className="sidenav">
+      <a href="#" onClick={closeNav} className="closebtn ">
+          <MenuIcon />
+        </a>
+        <ul>
+          <li>
+            <a href="#" onClick={closeNav}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={closeNav}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={closeNav}>
+              Clients
+            </a>
+          </li>
+          
+          <li>
+            <a href="#" onClick={closeNav}>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      <MenuIcon onClick={openNav} className="navIcon" />
+    </>
+  );
+};
+
+export default NavBar;
