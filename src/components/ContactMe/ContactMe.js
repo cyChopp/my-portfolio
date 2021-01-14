745e80c309c1ec7d3af2a3406b4088147e118c45
cyChopp <45396059+cyChopp@users.noreply.github.com>
2021-01-14 18:01:21 +0100

    Add contact info

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -3,15 +3,20 @@ import * as emailjs from "emailjs-com";
 import "./ContactMe.css";
 import {
   Button,
-    ButtonBase,
+  ButtonBase,
   Container,
   CssBaseline,
   Grid,
   makeStyles,
+  styled,
   TextField,
   Typography,
   withStyles,
 } from "@material-ui/core";
+
+import { ReactComponent as GithubSVG } from "../../icons/github.svg";
+import { ReactComponent as LinkedinSVG } from "../../icons/linkedIn.svg";
+import AlexanderLouttchenkoCV from '../../icons/AlexanderLouttchenkoCV.pdf'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -29,64 +34,60 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-  textField:{
-      root:{
-    '&.MuiInputBase-root':{
-        color:'red'
-    }
-      }
-  }
- 
-}));
-
-
-
-const ValidationTextField = withStyles({
+  textField: {
     root: {
-        '& .MuiOutlinedInput-notchedOutline':{
-            border:'2px solid #fff',
-        },
-        "& .MuiFormLabel-root.Mui-focused":{
-            color:'#0effea'
-        },
-        "& .MuiFormLabel-root":{
-            color:'#fff'
-        },
-        "& .MuiOutlinedInput-root": {
-            "&:hover fieldset": {
-                borderColor: "#0effea"
-              },
-        },
-        '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':{
-            borderColor:'#0effea'
-        },
-        '& .MuiInputBase-root':{
-            color:'#fff'
-        },
-      '& input:valid + fieldset': {
-        borderColor: '#0effea',
-        borderWidth: 2,
-      },
-      '& input:invalid + fieldset': {
-        borderColor: '#fff',
-        borderWidth: 2,
-      },
-      '& input:valid:focus + fieldset': {
-        borderColor: '#0effea',
-        borderLeftWidth: 6,
-        padding: '4px !important', // override inline-style
+      "&.MuiInputBase-root": {
+        color: "red",
       },
     },
-  })(TextField);
+  },
+}));
 
-  const ModefiedButton = withStyles({
-    '& .MuiButton-containedPrimary':{
-        fontWeight: '600',
-        color: '#000',
-        backgroundColor:' #0effea',
-    }
-  })(Button)
-  
+const ValidationTextField = withStyles({
+  root: {
+    "& .MuiOutlinedInput-notchedOutline": {
+      border: "2px solid #fff",
+    },
+    "& .MuiFormLabel-root.Mui-focused": {
+      color: "#0effea",
+    },
+    "& .MuiFormLabel-root": {
+      color: "#fff",
+    },
+    "& .MuiOutlinedInput-root": {
+      "&:hover fieldset": {
+        borderColor: "#0effea",
+      },
+    },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#0effea",
+    },
+    "& .MuiInputBase-root": {
+      color: "#fff",
+    },
+    "& input:valid + fieldset": {
+      borderColor: "#0effea",
+      borderWidth: 2,
+    },
+    "& input:invalid + fieldset": {
+      borderColor: "#fff",
+      borderWidth: 2,
+    },
+    "& input:valid:focus + fieldset": {
+      borderColor: "#0effea",
+      borderLeftWidth: 6,
+      padding: "4px !important", // override inline-style
+    },
+  },
+})(TextField);
+
+const ModefiedButton = withStyles({
+  "& .MuiButton-containedPrimary": {
+    fontWeight: "600",
+    color: "#000",
+    backgroundColor: " #0effea",
+  },
+})(Button);
 
 const ContactMe = () => {
   const classes = useStyles();
@@ -112,78 +113,125 @@ const ContactMe = () => {
 
     e.target.reset();
   };
-
+  const StyledSVGLinkedin = styled(LinkedinSVG)`
+  display: block;
+  margin: auto;
+`;
+const StyledSVGGithub = styled(GithubSVG)`
+display: block;
+margin: auto;
+`;
   return (
-    <>
-      <Container component="main" maxWidth="sm">
-        <div className={classes.paper}>
-          <Typography component="h1" variant="h5" className='contace_title'>
-            Contact me
-          </Typography>
-          <form className={classes.form} onSubmit={sendEmail}>
-            <Grid container spacing={4}>
-              <Grid item sm={12}>
-                <ValidationTextField
-                  variant="outlined"
-                  autoComplete="fname"
-                  name="name"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
+    <div className="contact_wrapper">
+      <div className="contact_formWrapper">
+        <Container component="main" maxWidth="sm" id="contact">
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h5" className="contace_title">
+              Contact me
+            </Typography>
+            <form className={classes.form} onSubmit={sendEmail}>
+              <Grid container spacing={4}>
+                <Grid item xs={12}>
+                  <ValidationTextField
+                    variant="outlined"
+                    autoComplete="fname"
+                    name="name"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <ValidationTextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="subject"
+                    label="Subject"
+                    name="subject"
+                    autoComplete="lname"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <ValidationTextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <ValidationTextField
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="message"
+                    label="Message"
+                    type="textarea"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <ValidationTextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="subject"
-                  label="Subject"
-                  name="subject"
-                  autoComplete="lname"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <ValidationTextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <ValidationTextField
-                multiline
-                 rows={4}
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="message"
-                  label="Message"
-                  type="textarea"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-            </Grid>
-            <ModefiedButton
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Send
-            </ModefiedButton>
-          </form>
+              <ModefiedButton
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Send
+              </ModefiedButton>
+            </form>
+          </div>
+        </Container>
+      </div>
+      <div className="contact_infoWrapper">
+        <div className="contact_info">
+          <div className='contact_infoSvg'>
+            <div>
+              LinkedIn :
+            </div>
+            <div className='contact_svgWrapper'>
+            <StyledSVGLinkedin className='contact_svg'/>
+            </div>
+          </div>
+          <div className='contact_infoSvg'>
+          <div>
+              GitHub :
+            </div>
+            <div className='contact_svgWrapper'>
+            <StyledSVGGithub className='contact_svg'/>
+            </div>
+
+          </div>
+          <div className='contact_infoSvg'>
+          <div>
+              Email :
+            </div>
+            <div className='contact_email'>
+            louttchenkoo@gmail.com
+            </div>
+          </div>
+          <div className='contact_infoSvg'>
+          <div>
+              CV :
+            </div>
+            <div className='contact_cv'>
+            <a href={AlexanderLouttchenkoCV} download='LouttchenkoCV.pdf' >Download CV</a>
+            </div>
+          </div>
+
+          
         </div>
-      </Container>
-    </>
+      </div>
+    </div>
   );
 };
 
